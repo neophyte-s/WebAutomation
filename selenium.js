ee333734da6a2a6cb1fa5ef1e5c8bfce885267cd
Swarnim <63869807+neophyte-s@users.noreply.github.com>
2020-05-03 22:06:25 +0530

    Update selenium.js

--- selenium.js
+++ selenium.js
@@ -13,6 +13,7 @@ let cFile=process.argv[2];
         let data=await fs.promises.readFile(cFile);
         let {url}=JSON.parse(data);
         await driver.get(url);
+        await loginHelper();
         // console.log("Website opened");
 
     }
@@ -21,3 +22,18 @@ let cFile=process.argv[2];
     }
 })
 ()
+
+async function loginHelper() {
+  await driver.manage().setTimeouts({ implicit: 30000, pageLoad: 50000 })
+  let data = await fs.promises.readFile(cFile);
+  let { url, pwd, user } = JSON.parse(data);
+  await driver.get(url);
+  let unInputWillBeFoundPromise = driver.findElement(swd.By.css("#input-1"));
+  let pwInputWillBeFoundPromise = driver.findElement(swd.By.css("#input-2"));
+  let  unNpwEl= await Promise.all([unInputWillBeFoundPromise, psInputWillBeFoundPromise]);
+  let uNameWillBeSendPromise = unNpsEl[0].sendKeys(user);
+  let pWillBeSendPromise = unNpwEl[1].sendKeys(pwd);
+  await Promise.all([uNameWillBeSendPromise, pWillBeSendPromise]);
+  let loginBtn = await driver.findElement(swd.By.css("button[data-analytics=LoginPassword]"));
+  await loginBtn.click();
+}
